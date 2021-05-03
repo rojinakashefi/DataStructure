049e20c924bb3dfdebaf32bd564382c3668679d6
rojinakashefi <rojina.kashefi@yahoo.com>
2021-05-03 09:06:39 +0430

    ShellSort edited

--- Sorting algorithm/src/ShellSort.java
+++ Sorting algorithm/src/ShellSort.java
@@ -1,31 +1,53 @@
 import java.util.Arrays;
 
+/**
+ * Shell sort
+ * in this sort we use insertion sort
+ * we don't use insertion sort from right and increase the sub array
+ * we choose a sub array based on our increment and sorting them
+ * and slowly increase of increment
+ * @author rojina
+ * @since April 2021
+ */
 public class ShellSort {
 
     int [] array;
     int increment;
-    InsertionSort insertionSort;
 
     public ShellSort(int []array,int increment){
         this.increment=increment;
         this.array=array;
         shellSort();
     }
+
+    /**
+     * creating our sub arrays based on our increment
+     * and calling insertion sort on our sub array
+     */
     public void shellSort(){
-        for(int i=0;i<increment;i++){
-            insertionSort(i);
+        while(increment>=1) {
+            //first it loops by increment
+            //when finished we reduce our increment
+            //in increment=1 is insertion sort
+            for (int i = 0; i < increment; i++) {
+                insertionSort(i);
+            }
+            increment=increment/2;
         }
-        //after the sublisted has been sorted
-        //we have a nearly sorted list
-        //we sort it as an normal insertionsort
-        insertionSort=new InsertionSort(array);
     }
+
+    /**
+     * sorting based on increment
+     * @param start of sub array we are going to sort
+     */
     public void insertionSort(int start){
         for(int i=start;i<array.length;i=i+increment){
             if(i+increment<array.length) {
                 for (int j = i + increment; j > 0; j = j - increment) {
                     if(j-increment>=0) {
                         if (array[j] < array[j - increment]) {
+                            System.out.println(array[j]);
+                            System.out.println(array[j-increment]);
                             swap(j, j - increment);
                         } else {
                             break;
@@ -38,6 +60,12 @@ public class ShellSort {
             }
         }
     }
+
+    /**
+     * swapping elements
+     * @param i as the first index of array we are going to swap
+     * @param j as the second index of array we are going to swap
+     */
     public void swap(int i,int j){
         int temp=array[i];
         array[i]=array[j];

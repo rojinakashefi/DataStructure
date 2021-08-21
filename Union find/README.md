fa38913afe0c80231164c2cd20a8d91633aa5696
rojinakashefi <rojina.kashefi@yahoo.com>
2021-08-21 14:53:31 +0430

    final changes to union find

--- Union find/README.md
+++ Union find/README.md
@@ -1,20 +1,43 @@
 # Union find
 
-U nion find is a data structure which keep track of elements which are split into two or more disjoint sets.it has two primary operations:
+Union find is a data structure which keep track of elements which are split into two or more disjoint sets.it has two primary operations:
 
-1-Find : will tell you what group an element belongs too
+1- Find : will tell you what group an element belongs too ( find the root of that group by following the parent nodes until self loop is reached.  )
 
-2-Union : merges two groups together.
+2- Union : merges two groups together (find root nodes of each group and if the root nodes are different make one of the root nodes be the parent of the other)
 
 we see union find in :
 
-- [ ] kruskal's minimum spanning tree algorithm
-- [ ] Grid percolation
-- [ ] Network connectivity
-- [ ] least common ancestor in trees
+- [ ] Kruskal's minimum spanning tree algorithm
+
+  - a minimum spanning tree is subset of the edges which connect all vertices in the graph with the minimal total edge cost.(it may not be unique)
+
+- [ ] Grid percolation :
+
+  - where there is bunch of dots on a grid and then we see if there is path from bottom of grid to top of grid (or vice versa)
+
+- [ ] Network connectivity :
+
+  - are two vertices in the graph connected to each other through series of edges
+
+- [ ] Least common ancestor in trees
+
 - [ ] Image processing
 
-Complexity:
+  ------
+
+  ### Implementation details
+
+  - Find and union operations:
+
+    1. First construct a mapping between your objects and the integer in the range [0, n).
+    2. Store union find information in an array. Each index has an associated object we can lookup through our mapping.
+
+  - path compression
+
+    ------
+
+    ### Complexity:
 
 |    Construction    | O(n) |
 | :----------------: | :--: |
@@ -24,3 +47,6 @@ Complexity:
 | check if connected | α(n) |
 |  count components  | O(1) |
 
+**The number of components is equal to the number of the roots remaing. **
+
+**Also,remark that the number of root nodes never increases.**
--- Union find/src/UnionFind.java
+++ Union find/src/UnionFind.java
@@ -1,31 +1,48 @@
+/**
+ * Implementing union find
+ */
 public class UnionFind {
+    //how many elements we have
     int size_all_elements;
+    //how many roots available
     int[] roots;
+    //size of each component
     int[] size_each_component;
+    //how many groups we have
     int number_component;
 
+    /**
+     * @param size as elements we have
+     */
     public UnionFind(int size) {
         if (size <= 0) {
             throw new IllegalArgumentException("size cant be zero");
         }
         this.size_all_elements = size;
+        //the components and roots we have are same at first
         roots = new int[size];
         size_each_component = new int[size];
         for (int i = 0; i < size; i++) {
             roots[i] = i;
+            // only themselves
             size_each_component[i] = 1;
         }
     }
 
+    /**
+     * finding an element is in a component or not
+     * @param element the element we are looking for
+     * @return root number of the element
+     */
     public int find(int element) {
-        if (roots[element] == element) {
-            return -1;
-        }
-        int root = element;
 
+        int root = element;
+        //if the element isn't the root keep going up
         while (roots[root] != root) {
             root = roots[element];
         }
+        //path compression
+        // and is what gives us amortized time complexity.
         while (roots[element] != root) {
             int next = roots[element];
             roots[element] = root;
@@ -34,27 +51,56 @@ public class UnionFind {
         }
         return root;
     }
-
+    /**
+     * check if two elements are in the same group or not
+     * by seeing their roots
+     */
     public boolean connected(int elm_1,int elm_2){
         return find(elm_1)==find(elm_2);
     }
+
+    /**
+     * size of group
+     * @param element as element in a group we are seeing its size
+     * @return size
+     */
     public int getSize_each_component(int element){
         return size_each_component[find(element)];
     }
+
+    /**
+     * how many elements we have
+     * @return size of elements
+     */
+
     public int getSize_all_elements(){
         return size_all_elements;
     }
+
+    /**
+     * how many groups we have
+     * @return number of components
+     */
     public int getNumber_component(){
         return number_component;
     }
+
+    /**
+     * merges two elements components if possible
+     * @param element_1 as element of group1
+     * @param element_2 as element of group2
+     */
     public void union(int element_1, int element_2) {
+        //if they have same roots means they are in same group
         if (connected(element_1,element_2)){
             return;
         }
         int root_1 =  find(element_1);
         int root_2 =  find(element_2);
+        //adds the group smaller to group bigger
         if(size_each_component[root_1]<=size_each_component[root_2]){
             size_each_component[root_2]+=size_each_component[root_1];
+            //make their root the same
             roots[root_1]=root_2;
             size_each_component[root_1]=0;
 
@@ -64,6 +110,7 @@ public class UnionFind {
             roots[root_2]=root_1;
             size_each_component[root_2]=0;
         }
+        // we have merged two groups to one groups(size of groups reduced)
         number_component-=1;
     }
 }
